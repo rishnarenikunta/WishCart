f8d07775396eefd164fea229a972a1e98857c40e
HivaHakimzadeh <hivahakimzadeh@yahoo.com>
2025-04-24 11:10:46 -0500

    signup page

---
+++ TaskB_folder/signupsend.php
@@ -0,0 +1,18 @@
+<?php
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $email = $_POST['email'];
+
+    $conn = new mysqli('localhost', 'root', '', 'taskb');
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    $sql = "INSERT INTO users (username, password, email) VALUES ('$username', '$password', '$email')";
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+    $conn->close();
+?>
