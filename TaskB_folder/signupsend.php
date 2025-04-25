37acb8bf91c38d96ab2496db14641d0f7a76846b
HivaHakimzadeh <hivahakimzadeh@yahoo.com>
2025-04-24 23:29:08 -0500

    connected frontend login and signup to backend

--- TaskB_folder/signupsend.php
+++ TaskB_folder/signupsend.php
@@ -5,14 +5,23 @@
 
     $conn = new mysqli('localhost', 'root', '', 'WishCart');
 
+
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
-    $sql = "INSERT INTO users (username, password, email) VALUES ('$username', '$password', '$email')";
-    if ($conn->query($sql) === TRUE) {
-        echo "New record created successfully";
+
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+    $sql = "INSERT INTO User (Username, Password, Email) VALUES (?, ?, ?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("sss", $username, $hashed_password, $email);
+
+    if ($stmt->execute()) {
+        header("Location: ./account.html");
+        exit();
     } else {
         echo "Error: " . $sql . "<br>" . $conn->error;
     }
+
     $conn->close();
 ?>
